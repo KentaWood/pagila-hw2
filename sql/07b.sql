42602889e05ceca09cc76fb9c1394531f1ed7131
KentaWood <you@example.com>
2024-02-26 16:54:57 -0800

    done

--- sql/07b.sql
+++ sql/07b.sql
@@ -2,4 +2,27 @@
  * This problem is the same as 07.sql,
  * but instead of using the NOT IN operator, you are to use a LEFT JOIN.
  */
+SELECT
+    f.title
+FROM
+    film f
+JOIN
+    inventory i ON f.film_id = i.film_id
+LEFT JOIN
+    (SELECT DISTINCT i.film_id
+     FROM
+        rental r
+        JOIN inventory i ON r.inventory_id = i.inventory_id
+        JOIN customer c ON r.customer_id = c.customer_id
+        JOIN address a ON c.address_id = a.address_id
+        JOIN city ci ON a.city_id = ci.city_id
+        JOIN country co ON ci.country_id = co.country_id
+     WHERE
+        co.country = 'United States') AS rented_in_us ON f.film_id = rented_in_us.film_id
+WHERE
+    rented_in_us.film_id IS NULL
+GROUP BY
+    f.title
+ORDER BY
+    f.title;
 
